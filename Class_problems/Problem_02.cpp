813feee28ca356ea0969284ac24fd9fb75c6200f
Rinchu <ritesshdawani@gamil.com>
2026-08-13 10:28:12 +0530

    Problem_02

---
+++ Class_problems/Problem_02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+double power(double n, int p);
+
+int main()
+{
+    int n,p;
+    cout<<"Raisig a no n to power p is the same as multipliying n by itself p time . Write a function called power() that takes double value for n, and an int value for p and return result as a doubke value .Use default arguments of 2 for p, so if argument is omited the number is squared . Write main() function to get values from user as input"<< endl;
+    cout<<"Enter a no = ";
+    cin>>n;
+    cout<<"enter power = ";
+    cin>>p;
+    cout<<"Ans is = "<<power(n,p)<<endl;
+    return 0;
+}
+
+double power(double n, int p){
+    int mul=1;
+    for (int i = 0; i < p; i++)
+    {
+        mul = mul*n;
+    }
+    return mul;
+}
